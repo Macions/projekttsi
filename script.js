3d0ec13793c6ed3dcfc9e12821c647c031bf1ef3
Maciej <maciejkaczarnecka@gmail.com>
2025-09-28 20:57:29 +0200

    v 2.1

--- script.js
+++ script.js
@@ -5,35 +5,32 @@ let footer = document.querySelector("footer");
 let main = document.querySelector("main");
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let lang = button.getAttribute("data-lang");
-        sections.forEach((section) => {
-            if (section.id === lang) {
-                section.classList.add("active");
-                main.classList.add("active");
-                footer.classList.add("active");
-            } else {
-                section.classList.remove("active");
-            }
-        });
-    });
-});
-
-closeButtons.forEach((closeButton) => {
-    closeButton.addEventListener("click", () => {
-        sections.forEach((section) => {
-            main.classList.remove("active");
-            footer.classList.remove("active");
-            section.classList.remove("active");
-        });
-    });
+	button.addEventListener("click", () => {
+		let lang = button.getAttribute("data-lang");
+		sections.forEach((section) => {
+			if (section.id === lang) {
+				section.classList.add("active");
+				main.classList.add("active");
+				footer.classList.add("active");
+			} else {
+				section.classList.remove("active");
+			}
+		});
+	});
 });
 
 let languageDetails = document.querySelectorAll(".languageDetails");
-languageDetails.forEach((detail) => {
-    detail.addEventListener("click", (event) => {
-        if (event.target === detail) {
-            detail.classList.remove("active");
-        }
+let contro="";
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        control = button.getAttribute("data-lang");
+		
+		setTimeout(() => {
+            languageDetails.forEach(detail => {
+				detail.style.opacity = "0";
+				
+			});
+        }, 2000);
+		detail.classList.remove("active");
     });
 });
